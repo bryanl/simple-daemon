00e1c1b76f6f93d2baa34057c82939a2c74f4644
bryan <bryan@dmac.local>
2008-06-17 16:27:29 -0400

    it is a private class method

--- lib/simple-daemon.rb
+++ lib/simple-daemon.rb
@@ -18,7 +18,7 @@ module SimpleDaemon
 
     private
 
-    def underscore(camel_cased_word)
+    def self.underscore(camel_cased_word)
       camel_cased_word.to_s.
       gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
       gsub(/([a-z\d])([A-Z])/,'\1_\2').
